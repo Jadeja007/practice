af63c1f5776482716f3407919645d151c2e1c389
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-03-28 19:02:58 +0530

    rest function

--- function.js
+++ function.js
@@ -7,21 +7,38 @@
 
 //Cube by passing function and Array
 
-const number = [0,1,2,5,10];
-const cube = cal(f, number)
+// const number = [0,1,2,5,10];
+// const cube = cal(f, number)
 
-function f(x){
-    return x*x*x;
-}
+// function f(x){
+//     return x*x*x;
+// }
 
-function cal(f,a){
-    const result = new Array(a.length);
-    for (let i=0; i<a.length; i++){
+// function cal(f,a){
+//     const result = new Array(a.length);
+//     for (let i=0; i<a.length; i++){
     
-        result[i] = f(a[i]);
-    }
-    return result
+//         result[i] = f(a[i]);
+//     }
+//     return result
+// }
+
+// console.log(cube)
+
+//Fibonacci series - WIP
+// function fib(){
+//     const a=0;
+//     const b=1;
+//     for (i=2, i>0, i++){
+//         return 
+//     }
+// }
+
+
+//Rest functions
+function multiply(multiplier, ...Arg){
+    const val = Arg.map(x => multiplier*x) ;
+    return val;
 }
-
-console.log(cube)
-
+const val = multiply(2,1,2,3);
+console.log(val)
