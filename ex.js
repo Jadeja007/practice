b1e265b42e523aed961e0d46b5702fbd0340aac0
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-03-28 14:19:34 +0530

    switch case

---
+++ ex.js
@@ -0,0 +1,21 @@
+//block scope
+
+var x = 1;
+{
+    var x = 3;
+}
+console.log(x);
+
+//switch case
+switch(condition){
+    case label1 :
+        statement1;
+        break;
+
+    case label2;
+        statement2;
+        break;
+    
+        default:
+            statement;
+}
