a15a1f7e1a373e03ab8d8466ec3f9b19b29b3d4a
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-03-29 15:51:53 +0530

    opertaors

--- Opertaor.js
+++ Opertaor.js
@@ -1,10 +1,36 @@
 //difference between ++x, x++
-let x = 3;
-for (let i=0; i<10; i++){
-    y=++x;
-    console.log(y);
+// let x = 3;
+// for (let i=0; i<10; i++){
+//     y=++x;
+//     console.log(y);
+// }
+// for (let i=0; i<10; i++){
+//     y=x++;
+//     console.log(y);
+// }
+
+//Assignment operator
+const obj = {};
+obj.x = 3;
+
+console.log(obj)
+const key = "y";
+obj[key] = 5;
+
+console.log(obj)
+
+//Bitwise Operator
+const a=10;
+const b=11;
+console.log("bitwise",a&b);
+
+//Short Circuit
+function A(){
+    console.log("called A")
+    return false;
 }
-for (let i=0; i<10; i++){
-    y=x++;
-    console.log(y);
+function B(){
+    console.log("called B");
+    return true
 }
+console.log("SC", A() && B() )
