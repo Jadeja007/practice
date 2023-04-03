c6320a490ceb4ebb71273895cd4c6b716bb593f2
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-04-03 19:23:10 +0530

    updated example

--- Array.js
+++ Array.js
@@ -50,7 +50,7 @@ const a = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 function isPrime(num){
       for (let i=2; num > i; i++){
-        if (num % 2 === 0){
+        if (num % i === 0){
             return false;
         }
       }
