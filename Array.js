d5e257802053745d6ed9d1e0a0e0beabd55e0f89
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-04-04 19:40:50 +0530

    Array methods with examples

--- Array.js
+++ Array.js
@@ -67,15 +67,15 @@ function filterSearch(are, query){
 
 console.log(filterSearch(fruits, "an"))
 
-// // .flat()
-// const arr12 = [1, 2];
-// // let result = arr12.flat();
-// console.log(typeof(arr12))
-// console.log([1,2,3,[4,5]].flat());
+// .flat()
+const arr12 = [1, 2, 3, 4];
+
+console.log(typeof(arr12))
+console.log([1,2,3,[4,5]].flat());
 
 // .flatmap()
-// const arr21 = [1,2,3];
-// console.log(arr21.flatMap(i => i === 2 ? [2,2] : 1))
+const arr21 = [1,2,3];
+console.log(arr21.flatMap(i => i === 2 ? [2,2] : 1))
 
 // .forEach()
 const items = ["i1", "i2"]
@@ -99,5 +99,91 @@ console.log(itm.indexOf("item2"));
 console.log(itm.indexOf("2"))
 console.log(itm.indexOf("item2", 3))
 
+// .map()
+const int = (elem) => parseInt(elem, 10)
+console.log(["1", "2", "3"].map(int), "map");  //=> alternative
+// console.log(["1", "2", "3"].map((i) => parseInt(i)))   //---> best way to remove float value
+// console.log(["1", "2", "3"].map(Number));
 
-console.log([1,2,3,[4,5]].flat());
+
+// .reduce()  ==> 
+const initialValue = 0;
+const sum = arr12.reduce((accum, curre) => accum+curre, initialValue);
+console.log("sum", sum);
+
+const getMax = (a, b) => Math.max(a,b)
+console.log([15, 10].reduce(getMax, 100));
+
+//ex- work without initial value
+const ar1 = [15, 16, 17, 18, 19];
+
+function reducer (accum, cur, index) {
+  const ret = accum + cur;
+  console.log(
+    `accumulator: ${accum}, currentValue: ${cur}, index: ${index}, returns: ${ret}`,
+  );
+return ret;
+}
+console.log(ar1.reduce(reducer));
+
+//work with initial value
+console.log(ar1.reduce((accum, cur) => accum + cur, 10))
+
+//
+const objects = [{ x: 1 }, { x: 2 }, { x: 3 }];
+console.log(objects.reduce((accum, cur) => accum + cur.x ,0))
+
+//
+const myArray = ["a", "b", "a", "b", "c", "e", "e", "c", "d", "d", "d", "d"];
+
+const updatedArr = myArray.reduce((accum, cur) =>{
+  if(!accum.includes(cur)){
+    return [...accum + cur]
+  }
+  return accum
+}, [ ])
+console.log(updatedArr)
+
+//reducerRight()
+console.log([1, 2, , 4].reduceRight((a, b) => a + b)); // --> it will start from right
+
+
+// .reverse()  --> it will also change initial array so be careful while using
+const item = ["item1", "item2", "item3"];
+// console.log(item.reverse());
+// console.log(item)
+
+const itemmm = [...item].reverse();
+console.log(itemmm)
+console.log(item)
+
+// .slice()     --> from start to end (end not included)
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+console.log(animals.slice(1));
+console.log(animals.slice(1,4));
+console.log(animals.slice(-3));
+console.log(animals.slice(2, -1))
+
+
+// .some()
+const even = (element) => element % 2 === 0 ;
+console.log(arr12.some(even))
+
+//ex
+function isBigger (element) {
+  return element > 10;
+}
+console.log([1,5,8].some(isBigger))
+
+//ex
+function checkAvailable(array ,arg ){
+     return array.some((current)=> arg === current)
+}
+console.log(checkAvailable(fruits, "apple"));
+
+// .sort()
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+console.log(months.sort())
+const array12 = [1, 30, 4, 21, 100000];
+console.log(array12.sort())
