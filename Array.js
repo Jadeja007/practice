bb41e276c21e0972f4ea7d6193c085611bc80d6f
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-04-05 16:10:39 +0530

    Array's Method

--- Array.js
+++ Array.js
@@ -22,6 +22,17 @@ console.log(array1.copyWithin(0,3))  //-->
 console.log(array1.copyWithin(0,2,4));  // --> 
 
 //.entries()   --> it will return new array iterator object that contain key/value pair for each index
+
+const a11= ["a", "b", "c"];
+
+for (const [elem, index] of a11.entries()){
+    console.log(`element: ${elem} , Index: ${index}`)
+}
+
+for (const element of a11.entries()){
+    console.log(element)
+}
+
 const arr = ['a', 'b', 'c'];
 const itr = arr.entries();
 console.log(itr.next().value)
@@ -73,9 +84,11 @@ const arr12 = [1, 2, 3, 4];
 console.log(typeof(arr12))
 console.log([1,2,3,[4,5]].flat());
 
-// .flatmap()
+// .flatmap()   ->> it takes callbackFn & Arg
 const arr21 = [1,2,3];
 console.log(arr21.flatMap(i => i === 2 ? [2,2] : 1))
+
+// console.log([1, 2].flatMap((i)=> i*2), "FlatMap")
 
 // .forEach()
 const items = ["i1", "i2"]
@@ -106,7 +119,7 @@ console.log(["1", "2", "3"].map(int), "map");  //=> alternative
 // console.log(["1", "2", "3"].map(Number));
 
 
-// .reduce()  ==> 
+// .reduce()  ==>  Replace with .filter().map()
 const initialValue = 0;
 const sum = arr12.reduce((accum, curre) => accum+curre, initialValue);
 console.log("sum", sum);
@@ -160,7 +173,7 @@ console.log(item)
 // .slice()     --> from start to end (end not included)
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 
-console.log(animals.slice(1));
+console.log("slice", animals.slice(1));
 console.log(animals.slice(1,4));
 console.log(animals.slice(-3));
 console.log(animals.slice(2, -1))
@@ -187,3 +200,111 @@ const months = ['March', 'Jan', 'Feb', 'Dec'];
 console.log(months.sort())
 const array12 = [1, 30, 4, 21, 100000];
 console.log(array12.sort())
+
+// .splice() --> this method will change initial Array   --Here we can add and delete array elements
+const month = ['Jan', 'March', 'April', 'June'];
+// console.log(month.splice(3));
+// const dd = month.splice(1, 2, 'Feb')
+const dd1 = month.splice(1, 2)
+console.log(month)
+
+// .fill()  ==>fill(value) /  fill(value, start)  /  fill(value, start, end)  --> this method will change initial Array
+
+const data = [2,3,4,5,6,7]
+// console.log( data.fill(5));
+// console.log(data.fill(2, 2,5))
+console.log(data.fill(10, -3, -1))
+console.log(data)
+
+
+// .find() 
+//const ar1 = [15, 16, 17, 18, 19];
+console.log(ar1.find((item)=>item < 19));
+
+//ex
+const inventory = [
+  { name: "apples", quantity: 2 },
+  { name: "fish", quantity: 1 },
+  { name: "bananas", quantity: 0 },
+  { name: "cherries", quantity: 5 },
+];
+
+function isCherries(fruits){
+  return fruits.name === "cherries"
+}
+console.log(inventory.find(isCherries))
+
+console.log(inventory.find(({name}) => name === "cherries"));  //-->By arrow Fn
+
+//ex
+const arrayfind = [0, 1, , , , 5, 6];
+
+arrayfind.find((elem, index) => console.log(`visited index ${index} its value ${elem}`))
+
+arrayfind.find((elem, index) => {
+  if (index === 0){
+    console.log("Deleting array[5]");
+    delete arrayfind[5];
+  }
+  console.log(`visited index ${index} its value ${elem}`);
+})
+
+
+//findIndex()
+console.log(ar1.findIndex(x => x > 15));
+console.log([1,2, ,3].findIndex(x => x===undefined))
+
+//findLast()   -->method iterates the array in reverse order 
+console.log(ar1.findLast(x => x>15));
+function findData(item){
+ return item.quantity < 2;
+}
+console.log(inventory.findLast(findData))
+
+//findLastIndex()
+console.log(ar1.findLastIndex( x => x>15))
+
+// .includes()   -> includes(searchElement)  /  includes(searchElement, fromIndex)
+console.log(ar1.includes(15));
+console.log(months.includes("Jan"));
+
+console.log([1, 2, 3].includes(2));
+console.log([1, 2, 3].includes(2, 1));
+console.log([1, 2, 3].includes(3, -2));  //--> for < 0 value entire array will be check
+console.log((["1", "2", "3"]).includes(2));
+
+// join()
+console.log(months.join());
+console.log(months.join(''));
+console.log(months.join('^'))
+
+//keys()
+console.log(Object.keys(ar1))
+console.log(Object.keys([1, ,3]));
+const den = [1, ,3, ,5];
+console.log([...den.keys()]) 
+
+// .toLocaleString()
+const ary = [1, 'a', new Date('21 Dec 1997 14:12:00 UTC')];
+console.log(ary.toLocaleString("en-US", { timeZone: 'UTC' }))
+
+const prices = ["￥7", 500, 8123, 12];
+console.log(prices.toLocaleString("ja-JP", { style: "currency", currency: "JPY" }));
+
+console.log([1, ,3].toLocaleString())
+
+// .pop()
+const mnth = [...months]
+console.log(mnth.pop())
+console.log(mnth);
+console.log(mnth.pop())
+console.log(mnth);
+console.log(months)
+
+
+// .push()    -> Return new array length & change initial array
+console.log(["1", "3"].push(['2']));
+
+// .shift()  -> Return delete first element & change initial array
+console.log([6,8,2,3,].shift())
+
