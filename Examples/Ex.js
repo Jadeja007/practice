68987cb71707b940e65bbb8c90de1fc1d4a0752a
kuldeep.jadeja <kuldeep.ror.expert@gmail.com>
2023-04-12 19:38:47 +0530

    Examples 12-04

--- Examples/Ex.js
+++ Examples/Ex.js
@@ -526,8 +526,268 @@ function moveLast3Char(str) {
 console.log(moveLast3Char("Python"));
 
 //63. middle three characters of a given string of odd length
-function stringMiddle3Char(str){
+function stringMiddle3Char(str) {
   return str.slice((str.length)/2 - 1, (str.length)/2 + 2)
 }
 console.log(stringMiddle3Char("Exercises"));
+
+//64. concatenate two strings and return the result. If the length of the strings are not same then remove the characters from the longer string.
+function concat2StrUpdate(str1, str2) {
+  if (str1.length === str2.length) {
+    return str1 + str2;
+  } if (str1.length > str2.length) {
+    return str1.substring((str1.length + str2.length)/2) + str2
+  } if (str2.length > str1.length) {
+    return str1 + str2.substring((str1.length + str2.length)/2) 
+  }
+
+}
+console.log(concat2StrUpdate("Python", "Javascript"));
+
+//65. string end with "Script". The string length must be greater or equal to 6. 
+function stringEnd(str) {
+  return (str.length >= 6) && (str.substring((str.length-6)) === "Script") ? true : false
+}
+console.log(stringEnd("Java Script"));
+
+// const stri = "javaScript"
+// console.log(stri.substring(stri.length-6))
+
+//69. compute the sum of three elements of a given array of integers of length 3.
+function sumArr(arr) {
+  return arr.reduce((accu, curr) => accu + curr)
+}
+console.log(sumArr([1,2,3,4,5]));
+
+//70. rotate array left
+function rotateArrLeft(arr) {
+  const first = arr.shift();
+  console.log(arr);
+  arr.push(first);
+  return arr
+}
+console.log(rotateArrLeft([1,2,3,4,5]));
+
+//71. end have 1
+function check1nLast(arr) {
+  return (arr[0] === 1 || arr[arr.length-1] === 1)
+}
+console.log(check1nLast([1,2,3,4,5]));
+
+//72. first and last elements are equal of a given array
+function sameArrFL(arr) {
+  return arr.length >= 1 && arr[0] === arr[arr.length-1]
+}
+console.log(sameArrFL([1,2,3,4,1]));
+
+//73. reverse the elements
+console.log([1,2,3,4,5].reverse());
+
+//74. find the larger value between the first or last and set all the other elements with that value
+function largeNumInArr(arr) {
+  return (arr[0] > arr[arr.length - 1]) ? arr.fill(arr[0]) : arr.fill(arr[arr.length - 1])
+}
+console.log(largeNumInArr([8,2,3,4,5]));
+
+//75. create a new array taking the middle elements of the two arrays of integer
+function midElementArr(arr1, arr2) {
+  const newArr= [];
+  if (arr1.length%2 !== 0 && arr2.length%2 !== 0) {
+    newArr.push(arr1[Math.floor(arr1.length/2)]);
+    newArr.push(arr1[Math.floor(arr2.length/2)]);
+    console.log(newArr)
+  } else {
+    console.log("enter odd length of array")
+  }
+}
+console.log(midElementArr([1,2,3,4,5], [1,2,3]));
+
+//77.  an array of integers of length 2 contains 1 or 3.
+function findInt(arr) {
+  if (arr.find(elem => elem === 1)) {
+    return true
+  } else if (arr.find(elem => elem === 3)) {
+    return true
+  } else {
+    return false;
+  }
+}
+console.log(findInt([2,3,4,5]));
+
+//78.  an array of integers should not contains 1 or 3.
+function findIntNot(arr) {
+  if (arr.find(elem => elem === 1)) {
+    return false
+  } else if (arr.find(elem => elem === 3)) {
+    return false
+  } else {
+    return true;
+  }
+}
+console.log(findIntNot([2,1,4,5]));
+
+//80. swap the first and last elements of a given array of integers.
+function swapArrFL(arr) {
+  // const newArr = [];
+  // newArr.push(arr.pop());
+  // newArr.push(arr);
+  // newArr.push(arr.shift());
+  // return newArr.flat()
+
+  [arr[0], arr[arr.length-1]] = [arr[arr.length-1], arr[0]];
+  return arr
+}
+console.log(swapArrFL([2,3,4,5]));
+
+//81. add two digits of a given positive integer of length two
+function addTwoInt(n) {
+  return n%10 + Math.floor(n/10)
+}
+console.log(addTwoInt(28));
+
+//82. add two positive integers without carry.
+
+
+
+//83. find the longest string from a given array of strings
+function findLongStr(arr) {
+//  const int =  arr.map(item => item.length);
+ return arr.reduce((input, current) => input.length > current.length ? input : current);
+}
+console.log(findLongStr(["a","aaa","aa","aaaaa"]));
+
+//84. replace each character of a given string by the next one in the English alphabet.
+function replaceEachChar(str) {
+  const ar = str.split("");
+  // for (let i=0; i < str.length; i++) {
+  //   ar[i] = String.fromCharCode(str.charCodeAt(i) + 1);
+  // }
+  //   return ar.join("").replace(/{/,"a");
+  return ar.map(elem => String.fromCharCode(elem.charCodeAt()+1)).join("").replace(/{/, "a")
+}
+console.log(replaceEachChar("abcxyz"));
+
+//85. 
+function crossSum(arr) {
+  // const first = [];
+  // const last = [];
+  // const output = [];
+  // for (let i=0; i<arr.length; i++) {
+  //   if(i%2 === 0){ 
+  //     first.push(arr[i])
+  //   } else {
+  //     last.push(arr[i])
+  //   }
+  // }
+  // output.push(first.reduce((accu,curr) => accu + curr));
+  // output.push(last.reduce((accu,curr) => accu + curr));
+
+  // return output
+
+  const initial = [0,0];
+  for (let i=0; i<arr.length; i++) {
+    if (i%2) initial[1] += arr[i]
+    else initial[0] += arr[i]
+  }
+  return initial
+}
+console.log(crossSum([2,1,4,5,6,9]));
+
+//87. same array y swapping
+// function sameArr(arr1, arr2) {
+//   if (arr1.length === arr2.length) {
+//     for (let i=0; i<arr1.length; i++) {
+//       if(arr1[i] === arr2[i]){
+//         return result = true
+//       }
+//     }
+//     return result
+//   }
+
+
+// }
+// console.log(sameArr([1,4,3], [1,2,3]))
+
+//88.
+
+
+//90. find the kth greatest element of a given array of integers
+function nHigherValue(n) {
+  const arr = [2,1,4,5,6,9];
+  arr.sort();
+  // console.log(arr)
+  return arr[n-1]
+}
+console.log(nHigherValue(4));
+
+//91.  find the maximum possible sum of some of its k consecutive numbers (numbers that follow each other in order.)
+function findMaxVal(arr) {
+  for (let i=0; i<arr.length-1; i++) {
+    arr[i] = arr[i] + arr[i+1]
+  }
+  return(Math.max(...arr.slice(0, -1)))
+}
+console.log(findMaxVal([2,1,4,5,6,9]));
+
+//92. absolute difference
+function findMaxValAbs(arr) {
+  for (let i=0; i<arr.length-1; i++) {
+    arr[i] = Math.abs(arr[i] - arr[i+1])
+  }
+  return (Math.max(...arr.slice(0, -1)))
+}
+console.log("cc", findMaxValAbs([1, 2, 3, 8, 9]));
+
+//93.
+
+//94. most repeated num
+// function repeatedNum(arr) {
+//   for (let i=0; i<arr.length; i++) {
+//     arr[i] 
+//   }
+// }
+// console.log(repeatedNum([9, 1, 2, 9, 3, 8, 8, 9]));
+
+//95. replace all the numbers with a specified number of a given array of integers.  1 -> 7
+function replaceVal(arr) {
+ for (let i=0; i<arr.length; i++) {
+  if (arr[i] === 1) {
+    arr[i] = 7;
+  }
+ }
+ return arr
+}
+console.log(replaceVal([1, 2, 3, 1, 8, 9]));
+
+//96. compute the sum of absolute differences of consecutive numbers of a given array of integers.
+function crossSumOfDiff(arr) {
+  // for (let i=0; i<arr.length; i++) {
+  //   arr[i] = Math.abs(arr[i] - arr[i+1]);
+  // }
+  // return arr.slice(0, -1).reduce((a, c) => a+c)
+  var ans = 0;
+  for (let i=1; i<arr.length; i++) {
+    ans += Math.abs(arr[i] - arr[i-1])
+    console.log(ans)
+  }  
+  return ans
+}
+console.log(crossSumOfDiff([1, 2, 3, 8, 9]));
+
+//97. 
+
+//98. minimum possible number of letters to make a given string written in the upper case or in the lower case.
+function upprLowerCase(str) {
+  var x=0;
+  var y=0;
+  for (let i=0; i<str.length; i++) {
+    if (/[A-Z]/.test(str[i])) {
+      x++;
+    } else {
+      y++;
+    }
+  }
+  return (x>y) ? str.toUpperCase() : str.toLowerCase();
+}
+console.log(upprLowerCase("WRIte"));
 
